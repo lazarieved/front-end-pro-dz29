c8af168178549e8bda8648f459e46254dcc54fcb
Ed <lazarev223@gmail.com>
2019-09-09 17:39:31 +0300

    add dz30

--- script.js
+++ script.js
@@ -1,38 +1,11 @@
-function makeRequest(url) {
-    return fetch(url).then(res => res.json())
-}
-
-makeRequest('https://dog.ceo/api/breeds/list/all')
-    .then(response => createList(response.message))
-
-function createList(dogs){
-    const fragment = document.createDocumentFragment();
-    const li = document.createElement('li');
-
-    for(dog in dogs){
-        var clone = li.cloneNode();
-        clone.innerText = dog;
-        fragment.appendChild(clone);
-        clickHandler(clone, dog)
-    }
-    const ul = document.getElementsByTagName('ul')[0];
-    ul.appendChild(fragment);
-}
-
-function clickHandler(li, name){
-    li.onclick = () => makeRequest(`https://dog.ceo/api/breed/${name}/images/random`)
-        .then(response => generateImage(response.message))
-}
-
-function generateImage(src){
-    const imgExist = document.querySelector('#images img')
-    if (!!imgExist){
-        imgExist.src = src;
-        return;
-    }
-    const img = document.createElement('img');
-    img.src = src;
-
-    const container = document.getElementById('images');
-    container.appendChild(img);
+function deepCopy (obj) {
+	let newObj = new obj.constructor();
+	Object.entries(obj).forEach(([key]) => {
+		if( typeof obj[key] === 'object' ){
+            newObj[key] = deepCopy(obj[key]);   
+		}else{
+			Object.defineProperty(newObj, key, Object.getOwnPropertyDescriptor(obj, key));
+		}
+	});
+	return newObj;
 }
